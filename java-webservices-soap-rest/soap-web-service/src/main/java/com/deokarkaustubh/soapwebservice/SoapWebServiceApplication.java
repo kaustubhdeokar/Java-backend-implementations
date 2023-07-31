4261cffb6d85cb69d902d53a96c77b8dd4e07df4
kaustubhdeokar <kaustubhd9@gmail.com>
2023-07-31 22:29:37 +0530

    who the hell even uses soap now

---
+++ java-webservices-soap-rest/soap-web-service/src/main/java/com/deokarkaustubh/soapwebservice/SoapWebServiceApplication.java
@@ -0,0 +1,14 @@
+package com.deokarkaustubh.soapwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
